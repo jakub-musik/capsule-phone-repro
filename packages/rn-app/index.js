14d369844bcb721c5e25b649e0c3ffebef41ab56
Jordy McNab <jordy@usecapsule.com>
2024-07-15 09:56:38 -0700

    fix: remove .git from rn-app to commit

---
+++ packages/rn-app/index.js
@@ -0,0 +1,6 @@
+import './shim';
+import {AppRegistry} from 'react-native';
+import App from '@rn-integration-examples/shared-ui';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ packages/rn-app/metro.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const nodeLibs = require('node-libs-react-native');
+
+const monorepoPackages = {
+  '@rn-integration-examples/shared-ui': path.join(__dirname, '../shared-ui'),
+};
+
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '..'),
+  ],
+  resolver: {
+    extraNodeModules: {
+      ...nodeLibs,
+      crypto: require.resolve('react-native-crypto'),
+      stream: require.resolve('readable-stream'),
+      process: require.resolve('process'),
+      ...monorepoPackages,
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true,
+      },
+    }),
+  },
+  sourceMapOptions: {
+    excludeSource: false, // Include source code in the source map
+    sourceMapEnabled: true, // Enable source map generation
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
